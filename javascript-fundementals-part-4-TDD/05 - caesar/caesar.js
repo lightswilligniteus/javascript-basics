3e8e82c019b76aab6e5dff5affd18195203f79db
kdaya <kdayathegamer@gmail.com>
2018-11-16 10:39:57 +1100

    refactor caesar code

--- javascript-fundementals-part-4-TDD/05 - caesar/caesar.js
+++ javascript-fundementals-part-4-TDD/05 - caesar/caesar.js
@@ -1,73 +1,31 @@
 const caesar = function(string, num) {
  let stringArray = string.split("");
  let caesarString = "";
+ let mod = num % 26;
 
-for(i = 0; i < stringArray.length; i++) {
-      let tempString = String.fromCharCode(getCharCode(stringArray[i], num));
-      caesarString += tempString;
-    }
- return caesarString;
+ for (let i = 0; i < stringArray.length; i++) {
+   let charCode = stringArray[i].charCodeAt(0)
+   if (checkAlphabet(charCode)) {
+     charCode += mod;
+     if (!checkAlphabet(charCode) || (charCode >= 90 && charCode - mod <= 90) || (charCode <= 122 && charCode - mod >= 122)) {
+       if (num < 0) {
+         charCode += 26;
+       } else {
+         charCode -= 26;
+       }
+     }
+   }
+   stringArray[i] = String.fromCharCode(charCode);
+   caesarString += stringArray[i];
+ }
+return caesarString;
 }
 
-function getCharCode(character, num) {
-  let charCode = character.charCodeAt(0);
-  let newCharCode = charCode + num;
-  let oldCharCode = charCode;
-
-  if (num > 26 || num < -26) {
-    num = num % 26;
-  }
-
-  if(checkSpecial(character)) {
-    charCode = charCode;
-  } else if (checkAlphabet(newCharCode)) {
-    charCode = charCode + num;
-  } else {
-    if (num > 0) {
-      charCode += num - 26;
-      if(checkUpperLower(oldCharCode, charCode)) {
-        charCode += 26;
-      } else if (checkSpecial(String.fromCharCode(charCode))) {
-        charCode += 26;
-      }
-
-    } else {
-      charCode += num + 26;
-      if(checkUpperLower(oldCharCode, charCode)) {
-        charCode -= 26;
-      } else if (checkSpecial(String.fromCharCode(charCode))) {
-        charCode -= 26;
-      }
-
-  }}
-  return charCode;
-}
-
-function checkSpecial(character) {
-  let specialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-  if(specialCharacter.test(character) || character == " ")  {
+function checkAlphabet(charCode) {
+  if (charCode >= 65 && charCode <= 90 || charCode >= 97 && charCode <= 122) {
     return true;
   } else {
     return false;
   }
 }
-
-function checkAlphabet(character) {
-  if((character > 97 && character < 122) || (character > 65 && character < 90)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function checkUpperLower(oldChar, newChar) {
-  if (oldChar <= 90 && newChar >= 97) {
-    return true;
-  } else if (oldChar >= 97 && newChar <= 90) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 module.exports = caesar
--- javascript-fundementals-part-4-TDD/removeFromArray/removeFromArray.js
+++ javascript-fundementals-part-4-TDD/removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
+  let iterate = arguments.length;
+  let args = [];
+  let array = [];
 
+  for (i = 0; i < iterate; i++) {
+    args[i] = arguments[i];
+  }
+
+  array = args.splice(0,1);
+
+  for (let i = 0; i < array.length; i++) {
+    if (args[i] == array[i]) {
+      array.spice(i, i+1);
+    }//let index = array.indexOf(arguments[i]);
+  }
+  console.log(array);
+  return array;
 }
 
 module.exports = removeFromArray
