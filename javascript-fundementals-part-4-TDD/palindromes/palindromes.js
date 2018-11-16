4f3ea45b7dc124a8fe0140337d3457b857b09c33
kdaya <kdayathegamer@gmail.com>
2018-11-16 15:51:11 +1100

    logic for palindromes exercise

--- javascript-fundementals-part-4-TDD/palindromes/palindromes.js
+++ javascript-fundementals-part-4-TDD/palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function(string) {
+  string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  string = string.replace(/ /g,"");
+  string = string.toLowerCase();
+  let reverseString = string.split("");
+  reverseString = reverseString.reverse();
+  reverseString = reverseString.join("");
 
+  return reverseString == string ? true: false;
 }
 
 module.exports = palindromes
