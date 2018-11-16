306076794379e1d51f55420e23443c02da471f48
kdaya <kdayathegamer@gmail.com>
2018-11-16 14:30:39 +1100

    logic for leapYears complete

--- javascript-fundementals-part-4-TDD/leapYears/leapYears.js
+++ javascript-fundementals-part-4-TDD/leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if ((year % 4 == 0 && !(year % 100 === 0)) || year == 1600 || year == 2000) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = leapYears
