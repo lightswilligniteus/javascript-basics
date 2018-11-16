667b1ba532bae83e71064c3f6dccada5289e2bb1
kdaya <kdayathegamer@gmail.com>
2018-11-16 14:12:20 +1100

    completed temp Conversion

--- javascript-fundementals-part-4-TDD/tempConversion/tempConversion.js
+++ javascript-fundementals-part-4-TDD/tempConversion/tempConversion.js
@@ -1,26 +1,22 @@
 const ftoc = function(far) {
   let num = (far - 32) * 5 / 9;
-  if (checkWhole(num)) {
-    return num;
-  } else {
-    return Number(num.toFixed(1));
-  }
+  return returnTemp(num);
 }
 
 const ctof = function(celc) {
   let num = celc * 9 / 5 + 32;
+  return returnTemp(num);
+}
+
+function checkWhole(num) {
+  ((num - Math.floor(num)) == 0) ? true: false;
+}
+
+function returnTemp(num) {
   if (checkWhole(num)) {
     return num;
   } else {
     return Number(num.toFixed(1));
-  }
-}
-
-function checkWhole(num) {
-  if ((num - Math.floor(num)) == 0) {
-    return true;
-  } else {
-    return false;
   }
 }
 
