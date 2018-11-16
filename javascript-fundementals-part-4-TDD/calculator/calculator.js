48afdbf3a5adb487822136db9c0b35b7c78d0718
kdaya <kdayathegamer@gmail.com>
2018-11-16 15:02:55 +1100

    completed logic for calculator exercise

--- javascript-fundementals-part-4-TDD/calculator/calculator.js
+++ javascript-fundementals-part-4-TDD/calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
-	
+function add (...args) {
+	let add = 0;
+	for(let i = 0; i < args.length; i++) {
+		add += Number(args[i]);
+	}
+	return add;
 }
 
-function subtract () {
-	
+function subtract (...args) {
+	let subtract = args[0];
+	for (let i = 1; i < args.length; i++) {
+		subtract -= Number(args[i]);
+	}
+	return subtract;
 }
 
-function sum () {
-	
+function sum (...args) {
+	let sum = 0;
+	let array = args[0];
+	for (let i = 0; i < array.length; i++) {
+		sum += Number(array[i]);
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (...args) {
+	let multiply = 1;
+	let array = args[0];
+	for (let i = 0; i < array.length; i++) {
+		multiply *= array[i];
+	}
+	return multiply;
 }
 
-function power() {
-	
+function power(...args) {
+	return Math.pow(args[0], args[1]);
 }
 
-function factorial() {
-	
+function factorial(...args) {
+	let factorial = 1;
+	for (let i = 0; i < args[0]; i++) {
+		factorial *= (args[0]-i);
+	}
+	return factorial;
 }
 
 module.exports = {
