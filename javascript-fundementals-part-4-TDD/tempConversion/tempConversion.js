049786cbcd2040542d113af0cb9561d8b853a1b4
kdaya <kdayathegamer@gmail.com>
2018-11-16 13:59:37 +1100

    logic completed

--- javascript-fundementals-part-4-TDD/tempConversion/tempConversion.js
+++ javascript-fundementals-part-4-TDD/tempConversion/tempConversion.js
@@ -1,9 +1,27 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let num = (far - 32) * 5 / 9;
+  if (checkWhole(num)) {
+    return num;
+  } else {
+    return Number(num.toFixed(1));
+  }
 }
 
-const ctof = function() {
+const ctof = function(celc) {
+  let num = celc * 9 / 5 + 32;
+  if (checkWhole(num)) {
+    return num;
+  } else {
+    return Number(num.toFixed(1));
+  }
+}
 
+function checkWhole(num) {
+  if ((num - Math.floor(num)) == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
