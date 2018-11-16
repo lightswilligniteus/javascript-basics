dd1c3e5387a1e66325a882d6e648695981534a5c
kdaya <kdayathegamer@gmail.com>
2018-11-16 14:32:12 +1100

    refactored if statement

--- javascript-fundementals-part-4-TDD/leapYears/leapYears.js
+++ javascript-fundementals-part-4-TDD/leapYears/leapYears.js
@@ -1,9 +1,5 @@
 const leapYears = function(year) {
-  if ((year % 4 == 0 && !(year % 100 === 0)) || year == 1600 || year == 2000) {
-    return true;
-  } else {
-    return false;
-  }
+  return ((year % 4 == 0 && !(year % 100 === 0)) || year == 1600 || year == 2000);
 }
 
 module.exports = leapYears
