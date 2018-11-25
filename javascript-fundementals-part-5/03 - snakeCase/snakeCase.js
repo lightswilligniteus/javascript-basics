a414d48fbda6ee0392fb8a9ae6f115297784ae67
kdaya <kdayathegamer@gmail.com>
2018-11-26 09:37:29 +1100

    complete snakeCase exercise

--- javascript-fundementals-part-5/03 - snakeCase/snakeCase.js
+++ javascript-fundementals-part-5/03 - snakeCase/snakeCase.js
@@ -1,5 +1,17 @@
-const snakeCase = function() {
+const snakeCase = function(string) {
+    string = string.replace(/\.\./g, " ");
 
+    if (string.indexOf(" ") < 0) {
+      string = string.replace(/([A-Z])/g, " $1");
+    }
+
+    return string
+      .trim()
+      .replace(/ /g,"_")
+      .replace(/[?=]/g,"")
+      .replace(/[-]/g, "_")
+      .replace(/[.,\/#!$%\^&\*;:{}=\`~()]/g,"")
+      .toLowerCase();
 }
 
 module.exports = snakeCase
